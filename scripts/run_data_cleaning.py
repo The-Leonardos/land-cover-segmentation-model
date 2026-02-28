a2defdb226fb917ab853395bbdabd3ef61b5136d
Leonhard Leung <Leonhardleung@gmail.com>
2026-02-28 09:15:22 +0800

    feat: numpy array saving
    added a feature where instead of saving the files as tiff files, numpy arrays are saved instead to avoid the computing demand of reading tiff files when training

--- scripts/run_data_cleaning.py
+++ scripts/run_data_cleaning.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+
+import numpy as np
 import rasterio as rio
 from tqdm import tqdm
 from landcover.utils import DataCleaning
@@ -26,16 +28,23 @@ def main():
     for split in splits:
         raw_masks_path = input_path / split / 'masks'
         clean_masks_path = output_path / split / 'masks'
+        raw_images_path = input_path / split / 'images'
+        clean_images_path = output_path / split / 'images'
 
-        # create clean masks path if missing
+        # create clean masks and images paths if missing
         clean_masks_path.mkdir(parents=True, exist_ok=True)
+        clean_images_path.mkdir(parents=True, exist_ok=True)
 
         # Get all mask files
         mask_files = (list(raw_masks_path.glob('*.tif')) + list(raw_masks_path.glob('*.tiff')))
         print(f'[{split}] Found {len(mask_files)} mask files to process')
 
+        # get all image files
+        image_files = (list(raw_images_path.glob('*.tif')) + list(raw_images_path.glob('*.tiff')))
+        print(f'[{split}] Found {len(image_files)} images to process')
+
         # clean all masks in the split
-        for mask_file in tqdm(mask_files, desc=f'Cleaning {split} split', leave=False):
+        for mask_file in tqdm(mask_files, desc=f'[MASKS] Cleaning {split} split', leave=False):
             # Read mask
             with rio.open(mask_file) as src:
                 mask = src.read(1)
@@ -54,10 +63,21 @@ def main():
             out_file = clean_masks_path / mask_file.name
             profile.update(dtype=rio.uint8, nodata=255)
 
-            with rio.open(out_file, "w", **profile) as dst:
-                dst.write(cleaned_mask.astype(rio.uint8), 1)
+            np.save(out_file.with_suffix('.npy'), cleaned_mask.astype(np.uint8))
 
         print(f'\nDone! Cleaned masks saved to {clean_masks_path}')
+
+        # save image tiff files as numpy
+        for image_file in tqdm(image_files, desc=f'[IMAGES] Saving {split} split as numpy arrays', leave=False):
+            # open image
+            with rio.open(image_file) as src:
+                image = src.read()
+
+            out_file = clean_images_path / image_file.name
+
+            np.save(out_file.with_suffix('.npy'), image.astype(np.uint8))
+
+        print(f'\nDone! Images saved to {clean_images_path} as numpy arrays')
 
     print(f"\nCleaning Complete!")
 
