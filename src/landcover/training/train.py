1bd577188454997dbb099ffdfdc5c3ce6604679c
Leonhard Leung <Leonhardleung@gmail.com>
2026-03-21 15:29:07 +0800

    updated train.py

--- src/landcover/training/train.py
+++ src/landcover/training/train.py
@@ -1,11 +1,11 @@
-import segmentation_models_pytorch as smp
 import torch
-
+import segmentation_models_pytorch as smp
+from landcover.utils.util import compute_iou
 
 def train(model_instance, data_loader, opt, loss_fn, device="cpu"):
     model_instance.train()
+    total_tp = total_fp = total_fn = total_tn = None
     running_loss = 0
-    running_iou = 0
 
     for images, masks in data_loader:
         images = images.to(device)
@@ -16,57 +16,26 @@ def train(model_instance, data_loader, opt, loss_fn, device="cpu"):
         outputs = model_instance(images)
 
         loss = loss_fn(outputs, masks)
-        iou = metric_out(outputs, masks)
+        tp, fp, fn, tn = compute_iou(outputs, masks)
 
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model_instance.parameters(), 1.0)
         opt.step()
 
         running_loss += loss.item()
-        running_iou += iou
+
+        if total_tp is None:
+            total_tp = tp
+            total_fp = fp
+            total_fn = fn
+            total_tn = tn
+        else:
+            total_tp += tp
+            total_fp += fp
+            total_fn += fn
+            total_tn += tn
 
     avg_loss = running_loss / len(data_loader)
-    avg_iou = running_iou / len(data_loader)
+    m_iou = smp.metrics.iou_score(total_tp, total_fp, total_fn, total_tn, reduction="macro")
 
-    return avg_loss, avg_iou
-
-def test(model_instance, data_loader, loss_fn, device="cpu"):
-    model_instance.eval()
-    running_loss = 0
-    running_iou = 0
-    
-    with torch.no_grad():
-        for images, masks in data_loader:
-            images = images.to(device)
-            masks = masks.to(device)
-
-            outputs = model_instance(images)
-
-            loss = loss_fn(outputs, masks)
-            iou = metric_out(outputs, masks)
-
-            running_loss += loss.item()
-            running_iou += iou
-
-    avg_loss = running_loss / len(data_loader)
-    avg_iou = running_iou / len(data_loader)
-
-    return avg_loss, avg_iou
-
-
-def metric_out(outputs, masks):
-    preds = torch.argmax(outputs, dim=1).long()
-    masks = masks.long()
-
-    tp, fp, fn, tn = smp.metrics.get_stats(
-        preds,
-        masks,
-        mode='multiclass',
-        num_classes=9,
-    )
-
-    iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction='macro')
-
-    return iou
-
-
+    return avg_loss, m_iou.item()
