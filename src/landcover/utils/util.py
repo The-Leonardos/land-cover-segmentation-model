500c41d9f5f8723996e1ef34755e477cf2df40e3
Leonhard Leung <Leonhardleung@gmail.com>
2026-03-25 17:25:09 +0800

    updated utils

--- src/landcover/utils/util.py
+++ src/landcover/utils/util.py
@@ -1,12 +1,19 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import segmentation_models_pytorch as smp
+import numpy as np
+from landcover import DATA_PATH
 
 
 def get_loss_fn(dice_weight=0.5, ce_weight=0.5):
+    weights = np.load(DATA_PATH / "misc" / "class_weights.npy")
+    class_weights = torch.tensor(weights, dtype=torch.float32)
+    class_weights = class_weights.cuda() if torch.cuda.is_available() else class_weights
+
     dice_loss = smp.losses.DiceLoss(mode='multiclass', ignore_index=255)
-    ce_loss = nn.CrossEntropyLoss(ignore_index=255)
+    ce_loss = nn.CrossEntropyLoss(weight=class_weights, ignore_index=255)
 
     def loss(pred, target):
         return (dice_weight * dice_loss(pred, target)) + ce_weight * ce_loss(pred, target)
@@ -28,3 +35,23 @@ def compute_iou(outputs, masks):
     )
 
     return tp, fp, fn, tn
+
+def pad_image(image, patch_size):
+    if (len(image.shape) == 4):
+        _, _, h, w = image.shape
+    else:
+        _, h, w = image.shape
+
+    pad_h = (patch_size - h % patch_size) % patch_size
+    pad_w = (patch_size - w % patch_size) % patch_size
+
+    padded = F.pad(image, (0, pad_w, 0, pad_h))
+
+    return padded, pad_h, pad_w
+
+def unpad_image(pred, pad_h, pad_w):
+    if pad_h > 0:
+        pred = pred[:, :, :-pad_h, :]
+    if pad_w > 0:
+        pred = pred[:, :, :, :-pad_w]
+    return pred
