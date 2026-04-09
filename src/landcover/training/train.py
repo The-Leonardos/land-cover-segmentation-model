5aeaf78573f530dd987cc29c1f3b7cdbdd4320c5
Leonhard Leung <Leonhardleung@gmail.com>
2026-04-09 18:32:11 +0800

    updated train function

--- src/landcover/training/train.py
+++ src/landcover/training/train.py
@@ -2,7 +2,7 @@ import torch
 import segmentation_models_pytorch as smp
 from landcover.utils.util import compute_iou
 
-def train(model_instance, data_loader, opt, loss_fn, device="cpu"):
+def train(model_instance, data_loader, opt, loss_fn, device="cpu", conf_matrix=None):
     model_instance.train()
     total_tp = total_fp = total_fn = total_tn = None
     running_loss = 0
@@ -16,13 +16,23 @@ def train(model_instance, data_loader, opt, loss_fn, device="cpu"):
         outputs = model_instance(images)
 
         loss = loss_fn(outputs, masks)
-        tp, fp, fn, tn = compute_iou(outputs, masks)
+        tp, fp, fn, tn = compute_iou(outputs, masks, model_instance.out_classes)
+
+        if conf_matrix is not None:
+            preds = torch.argmax(outputs, dim=1)
+            valid = masks != 255
+            conf_matrix.update(preds[valid], masks[valid])
 
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model_instance.parameters(), 1.0)
         opt.step()
 
         running_loss += loss.item()
+
+        tp = tp.sum(dim=0)
+        fp = fp.sum(dim=0)
+        fn = fn.sum(dim=0)
+        tn = tn.sum(dim=0)
 
         if total_tp is None:
             total_tp = tp
