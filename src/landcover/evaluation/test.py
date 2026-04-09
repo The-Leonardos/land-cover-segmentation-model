1831fcc69acfa999e20f4af6193960ac7f6af329
Leonhard Leung <Leonhardleung@gmail.com>
2026-04-09 18:31:52 +0800

    updated test function

--- src/landcover/evaluation/test.py
+++ src/landcover/evaluation/test.py
@@ -2,7 +2,7 @@ import torch
 import segmentation_models_pytorch as smp
 from landcover.utils.util import compute_iou, pad_image, unpad_image
 
-def test(model_instance, data_loader, loss_fn, patch_size, device="cpu"):
+def test(model_instance, data_loader, loss_fn, patch_size=256, device="cpu", conf_matrix=None):
     model_instance.eval()
     total_tp = total_fp = total_fn = total_tn = None
     running_loss = 0
@@ -13,28 +13,46 @@ def test(model_instance, data_loader, loss_fn, patch_size, device="cpu"):
             masks = masks.to(device)
 
             padded, pad_h, pad_w = pad_image(images, patch_size)
+            _, _, H_pad, W_pad = padded.shape
 
-            outputs = model_instance(padded)
+            logits_full = torch.zeros(
+                (images.shape[0], model_instance.out_classes, H_pad, W_pad),
+                device=device
+            )
+            count_map = torch.zeros((images.shape[0], 1, H_pad, W_pad), device=device)
 
-            outputs = unpad_image(outputs, pad_h, pad_w)
+            stride = patch_size // 2
 
-            loss = loss_fn(outputs, masks)
-            tp, fp, fn, tn = compute_iou(outputs, masks)
+            for i in range(0, H_pad - patch_size + 1, stride):
+                for j in range(0, W_pad - patch_size + 1, stride):
+                    patch = padded[:, :, i:i + patch_size, j:j + patch_size]
+                    outputs = model_instance(patch)
 
+                    logits_full[:, :, i:i + patch_size, j:j + patch_size] += outputs
+                    count_map[:, :, i:i + patch_size, j:j + patch_size] += 1
+
+            logits_full = logits_full / count_map
+            logits_full = unpad_image(logits_full, pad_h, pad_w)
+
+            loss = loss_fn(logits_full, masks)
             running_loss += loss.item()
 
+            pred_mask = torch.argmax(logits_full, dim=1)
+            tp, fp, fn, tn = compute_iou(logits_full, masks, model_instance.out_classes)
+
+            if conf_matrix is not None:
+                valid = masks != 255
+                conf_matrix.update(pred_mask[valid], masks[valid])
+
             if total_tp is None:
-                total_tp = tp
-                total_fp = fp
-                total_fn = fn
-                total_tn = tn
+                total_tp, total_fp, total_fn, total_tn = tp, fp, fn, tn
             else:
                 total_tp += tp
                 total_fp += fp
                 total_fn += fn
                 total_tn += tn
 
-    avg_loss = running_loss / len(data_loader)
-    m_iou = smp.metrics.iou_score(total_tp, total_fp, total_fn, total_tn, reduction="macro")
+        avg_loss = running_loss / len(data_loader)
+        m_iou = smp.metrics.iou_score(total_tp, total_fp, total_fn, total_tn, reduction="macro")
 
     return avg_loss, m_iou.item()
