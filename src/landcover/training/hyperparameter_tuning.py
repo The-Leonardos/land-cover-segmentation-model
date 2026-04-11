878ed26ec60755e19db4d351c0ae7a79dc850354
Leonhard Leung <Leonhardleung@gmail.com>
2026-04-11 22:20:45 +0800

    updated hyperparameter_tuning.py

--- src/landcover/training/hyperparameter_tuning.py
+++ src/landcover/training/hyperparameter_tuning.py
@@ -3,12 +3,13 @@ import optuna
 from torch.utils.data import DataLoader
 from landcover.datasets import LandCoverDataset
 from landcover.models.model import LandCoverModel
-from landcover.utils import get_optimizer, get_loss_fn
+from landcover.utils import get_optimizer, get_loss_fn, seed_everything, seed_worker
 from landcover.training.train import train
 from landcover.evaluation.test import test
 from landcover import DATA_PATH
 from tqdm import tqdm
 import wandb
+from landcover import NUM_CLASSES
 
 
 class HyperparameterTuning:
@@ -18,8 +19,6 @@ class HyperparameterTuning:
         self.encoder = encoder
         self.version = version
         self.device = device
-        self.train_dataset = LandCoverDataset((DATA_PATH / "dataset" / "clean" / "train"), train_mode=True, pre_load=True)
-        self.validation_dataset = LandCoverDataset((DATA_PATH / "dataset" / "clean" / "test"), train_mode=False, pre_load=True)
 
     def run(self):
         # perform optuna study
@@ -44,7 +43,7 @@ class HyperparameterTuning:
             project="land-cover-mapping",
             name=f"{self.encoder}/tuning/{self.version}/best_trial",
             config=best_params,
-            notes=f"Best trial achieved test IoU of {best_value:.4f}",
+            notes=f"Best trial achieved validation IoU of {best_value:.4f}",
             dir=str(DATA_PATH),
             reinit=True
         )
@@ -57,26 +56,23 @@ class HyperparameterTuning:
         return study.trials_dataframe(), study.best_params
 
     def _objective(self, trial):
+        # apply seeding
+        seed_everything(42 + trial.number)
+        g = torch.Generator()
+        g.manual_seed(42)
+
         # hyperparameters
-        lr = trial.suggest_float("lr", 1e-5, 5e-3, log=True)
-        weight_decay = trial.suggest_float("weight_decay", 1e-6, 1e-4, log=True)
-        encoder_out_stride = trial.suggest_categorical("output_stride", [8, 16])
-        encoder_depth = trial.suggest_categorical("encoder_depth", [4, 5])
-        aspp_dropout = trial.suggest_float("aspp_dropout", 0.0, 0.5)
-        decoder_channels = trial.suggest_categorical("decoder_channels", [64, 128, 256, 512])
-        decoder_atrous_rates = trial.suggest_categorical(
-            "decoder_atrous_rates",
-            [
-                (6, 12, 18),
-                (12, 24, 36),
-                (3, 6, 9),
-                (12, 18, 24)
-            ]
-        )
-        decoder_aspp_separable = trial.suggest_categorical("decoder_aspp_separable", [True, False])
-        batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64])
-        dice_weight = trial.suggest_float("dice_weight", 0.4, 0.8)
-        patch_size = trial.suggest_categorical("patch_size", [256, 512])
+        lr = trial.suggest_float("lr", 5e-5, 2e-4, log=True)
+        weight_decay = trial.suggest_float("weight_decay", 1e-4, 4e-4, log=True)
+        decoder_atrous_rates = trial.suggest_categorical("decoder_atrous_rates", [(12, 18, 24)])
+        decoder_aspp_separable = trial.suggest_categorical("decoder_aspp_separable", [False])
+        decoder_channels = trial.suggest_categorical("decoder_channels", [128, 256])
+        encoder_depth = trial.suggest_categorical("encoder_depth", [5])
+        encoder_out_stride = trial.suggest_categorical("output_stride", [16])
+        patch_size = trial.suggest_categorical("patch_size", [256])
+        batch_size = trial.suggest_categorical("batch_size", [16])
+        aspp_dropout = trial.suggest_float("aspp_dropout", 0.2, 0.4)
+        dice_weight = trial.suggest_float("dice_weight", 0.4, 0.6)
 
         # initialize wandb run
         wandb.init(
@@ -101,18 +97,18 @@ class HyperparameterTuning:
         )
 
         # dataloaders
-        self.train_dataset.set_patch_size(patch_size)
-        self.validation_dataset.set_patch_size(patch_size)
+        train_dataset = LandCoverDataset((DATA_PATH / "dataset" / "clean" / "train"), patch_size=patch_size, train_mode=True, pre_load=False)
+        validation_dataset = LandCoverDataset((DATA_PATH / "dataset" / "clean" / "test"), patch_size=patch_size, train_mode=False, pre_load=False)
 
-        train_loader = DataLoader(self.train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-        validation_loader = DataLoader(self.validation_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, worker_init_fn=seed_worker, generator=g)
+        validation_loader = DataLoader(validation_dataset, batch_size=1, shuffle=False, num_workers=1, pin_memory=True, worker_init_fn=seed_worker, generator=g)
 
         # model
         model = LandCoverModel(
             encoder_name=self.encoder,
             encoder_weights="imagenet",
             in_channels=4,
-            out_classes=9,
+            out_classes=NUM_CLASSES,
             encoder_output_stride=encoder_out_stride,
             encoder_depth=encoder_depth,
             decoder_aspp_dropout=aspp_dropout,
@@ -132,6 +128,7 @@ class HyperparameterTuning:
 
         # training loop
         best_m_iou = 0
+        train_m_iou_at_best_val = 0
         for epoch in tqdm(range(self.epochs), desc=f"Trial {trial.number}", leave=False):
             avg_train_loss, train_m_iou = train(model, train_loader, optimizer, loss_fn, device=self.device)
             avg_val_loss, val_m_iou = test(model, validation_loader, loss_fn, patch_size, device=self.device)
@@ -144,17 +141,19 @@ class HyperparameterTuning:
 
             if val_m_iou > best_m_iou:
                 best_m_iou = val_m_iou
+                train_m_iou_at_best_val = train_m_iou
 
             wandb.log({
                 "Train Loss": avg_train_loss,
                 "Validation Loss": avg_val_loss,
                 "Train mIoU": train_m_iou,
                 "Validation mIoU": val_m_iou,
-                "Best mIoU": best_m_iou,
+                "Best Validation mIoU": best_m_iou,
+                "Train mIoU at Best Validation": train_m_iou_at_best_val,
                 "Epoch": epoch
             })
 
-            trial.report(best_m_iou, epoch)
+            trial.report(val_m_iou, epoch)
             if trial.should_prune():
                 wandb.summary["Pruned"] = True
                 wandb.finish()
@@ -164,5 +163,6 @@ class HyperparameterTuning:
 
         torch.cuda.empty_cache()
         wandb.summary["Best Validation mIoU"] = best_m_iou
+        wandb.summary["Train mIoU at Best Validation"] = train_m_iou_at_best_val
         wandb.finish()
         return best_m_iou
