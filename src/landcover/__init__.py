ec0e9f0fcca869c0b85b122e61d9c8fa41b0c1cb
Leonhard Leung <Leonhardleung@gmail.com>
2026-04-11 21:29:39 +0800

    updated variables

--- src/landcover/__init__.py
+++ src/landcover/__init__.py
@@ -1,5 +1,14 @@
 from pathlib import Path
 
+# Directories
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 SOURCE_ROOT = Path(__file__).resolve().parent.parent
 DATA_PATH = PROJECT_ROOT / 'data'
+
+# Classes
+NUM_CLASSES = 7
+MINORITY_CLASSES = [0, 6, 3, 4]
+
+# Probability of Patch Types and Augmentation
+PATCH_SAMPLING_PROBS = {"minority": 0.7, "random": 0.3}
+AUGMENTATION_PROBS = 0.5
