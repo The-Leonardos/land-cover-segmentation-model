f50f79540564d8e58776cbea6f456fc7c7b20140
Leonhard Leung <Leonhardleung@gmail.com>
2026-04-11 20:43:56 +0800

    feat: added library for sliding window inference

--- src/landcover/evaluation/test.py
+++ src/landcover/evaluation/test.py
@@ -1,38 +1,26 @@
 import torch
 import segmentation_models_pytorch as smp
+from monai.inferers import sliding_window_inference
 from landcover.utils.util import compute_iou, pad_image, unpad_image
 
 def test(model_instance, data_loader, loss_fn, patch_size=256, device="cpu", conf_matrix=None):
     model_instance.eval()
-    total_tp = total_fp = total_fn = total_tn = None
+
     running_loss = 0
+    total_tp = total_fp = total_fn = total_tn = None
 
     with torch.no_grad():
         for images, masks in data_loader:
             images = images.to(device)
             masks = masks.to(device)
 
-            padded, pad_h, pad_w = pad_image(images, patch_size)
-            _, _, H_pad, W_pad = padded.shape
-
-            logits_full = torch.zeros(
-                (images.shape[0], model_instance.out_classes, H_pad, W_pad),
-                device=device
+            logits_full = sliding_window_inference(
+                inputs=images,
+                roi_size=(patch_size, patch_size),
+                sw_batch_size=4,
+                predictor=model_instance,
+                overlap=0.5
             )
-            count_map = torch.zeros((images.shape[0], 1, H_pad, W_pad), device=device)
-
-            stride = patch_size // 2
-
-            for i in range(0, H_pad - patch_size + 1, stride):
-                for j in range(0, W_pad - patch_size + 1, stride):
-                    patch = padded[:, :, i:i + patch_size, j:j + patch_size]
-                    outputs = model_instance(patch)
-
-                    logits_full[:, :, i:i + patch_size, j:j + patch_size] += outputs
-                    count_map[:, :, i:i + patch_size, j:j + patch_size] += 1
-
-            logits_full = logits_full / count_map
-            logits_full = unpad_image(logits_full, pad_h, pad_w)
 
             loss = loss_fn(logits_full, masks)
             running_loss += loss.item()
